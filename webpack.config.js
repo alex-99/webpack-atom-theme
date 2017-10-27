edd737b0bca030d796bbeefe068819f3786d4d64
Alexander Griffin <alexander.griffin99@hotmail.com>
2017-10-26 21:37:24 -0400

    initial webpack setup
    created basic wbpack project and added jszip which should be useful later

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [{
+      test: /\.sass$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader"
+      }]
+    }]
+    // {
+    //   test: /\.js$/,
+    //   use: [{
+    //     loader: "babel-loader",
+    //     options: {
+    //       presets: ["env"]
+    //     }
+    //   }]
+    // }]
+  }
+};
